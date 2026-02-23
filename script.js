12c323ef685db23720dc345022fe868191df068b
Kylie Seto <kylie.seto@gmail.com>
2026-02-22 18:28:45 -0800

    initial card state complete

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("btn-lm");
+button.addEventListener("click", expand)
+function expand = 
+// Write a JavaScript function that toggles the visibility of the additional details when the "More Info" button is clicked.
+// Implement logic to ensure that the button's text changes to "Less Info" when the additional details are visible and reverts back to "More Info" when hidden.
